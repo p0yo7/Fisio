661745c11b5a0bf295a3a7ab58f9c8506c30cb8d
Cortes Oscar <cortes.o.inter@e80group.com>
2024-03-22 15:09:40 -0600

    updated routes.js

--- API/routes.js
+++ API/routes.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const controller = require('./controller.js');
 const router = express.Router();
-
-router.get('/api/getLogs/', controller.getLogs);
-router.post('/api/postLogs/', controller.postLogs);
+// ROUTE GETS
+router.get('/api/login/', controller.login);
+router.get('/api/getProgressTherapist/', controller.getProgressTherapist);
+router.get('/api/getProgressPatient/', controller.getProgressPatient);
+// ROUTE POSTS
+router.post('/api/createUser/', controller.createUser);
 
 module.exports = router;
